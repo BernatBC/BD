4ab531d733650204ffed2772e44103702b832da0
Miquel <77343599+miquelt9@users.noreply.github.com>
2022-01-26 18:41:22 +0100

    Delete 1-ElLlenguatgeSQL1 directory

--- 1-ElLlenguatgeSQL1/Pregunta1.sql
+++
@@ -1,3 +0,0 @@
-select distinct d.num_dpt, d.nom_dpt
-from departaments d, empleats e
-where e.sou >= 200000 and d.ciutat_dpt in ('MADRID') and e.num_dpt = d.num_dpt
--- 1-ElLlenguatgeSQL1/Pregunta2.sql
+++
@@ -1,3 +0,0 @@
-select d.nom_dpt , p.nom_proj
-from empleats e natural inner join departaments d natural inner join projectes p
-where e.num_empl = 2
