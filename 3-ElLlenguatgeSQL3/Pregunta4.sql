5c3547065a0de65c0c7bd8f41c29c745a8e5f990
Miquel <77343599+miquelt9@users.noreply.github.com>
2022-01-26 18:41:57 +0100

    Delete 3-ElLlenguatgeSQL3 directory

--- 3-ElLlenguatgeSQL3/Pregunta4.sql
+++
@@ -1,6 +0,0 @@
-insert into professors VALUES('1', 'Marc', '999', 3000);
-insert into despatxos VALUES('A1', '201', 21);
-insert into despatxos VALUES('A1', '202', 21);
-insert into assignacions VALUES('1', 'A1', '201', 70, 80);
-insert into assignacions VALUES('1', 'A1', '202', 70, 80);
-insert into assignacions VALUES('1', 'A1', '202', 90, 100);
